edbb4bcdc90679dfa5326369bb072571eb896f0e
Nithin <nithin.g@ignitesol.com>
2016-08-25 12:21:16 +0530

    Init commit

---
+++ lib/tree.go
@@ -0,0 +1,13 @@
+package lib
+
+import (
+	"fmt"
+)
+
+func Test() {
+	fmt.Println("howdy partner!")
+}
+
+func main() {
+	fmt.Println("This should ideally never be run")
+}
---
+++ tree_prac.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	lib "tree_prac/lib"
+)
+
+func main() {
+	fmt.Printf("%s", lib.MakeNode(12))
+}
