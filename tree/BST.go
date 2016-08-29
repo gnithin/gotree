d1de7bec430889872e3657f0014fd7edb3200489
gnithin <nithin.linkin@gmail.com>
2016-08-29 18:24:34 +0530

    Add basic structure of removal of node in BST

--- tree/BST.go
+++ tree/BST.go
@@ -63,3 +63,65 @@ func (self *Tree) getNodeBST(node *Node, key interface{}) *Node {
 
 	return self.getNodeBST(node.link[dirn], key)
 }
+
+// Removing an element from a BST
+func (self *Tree) removeBST(node *Node, key interface{}) bool {
+	nodeResp := self.getNodeBST(self.root, key)
+	if nodeResp == nil {
+		return false
+	}
+
+	// This will only work if there's a parent key
+	parentNode, IsParentKey := nodeResp.link["parent"]
+	if !keyExists {
+		panic("The parent key in the nodes must be enabled for the removal to work!!! The alternative has not been implemented!")
+	}
+	var parentDirn string
+	parentDirn = ""
+	if parentNode != nil {
+		if parentNode.link["left"] == node {
+			// it's the left kid
+			parentDirn = "left"
+		} else {
+			parentDirn = "right"
+		}
+	}
+
+	leftNode, IsLeftKey := nodeResp.link["left"]
+	rightNode, IsRightKey := nodeResp.link["right"]
+
+	if !IsLeftKey || !IsRightKey {
+		// TODO: Better error message
+		panic("This shouldn't be happening!!!! Arrrrgh!!!!")
+	}
+
+	if leftNode == nil && rightNode == nil {
+		// If it's the root, then just nuke it
+		if parentNode == nil {
+			self.root = nil
+		} else {
+			// Remove this thing from the parent.
+			// Find out what child of the parent it is
+			parentNode.link[parentDirn] = nil
+		}
+		// TODO: DEstroy node
+	} else if leftNode == nil || rightNode == nil {
+		nonEmptyNode := leftNode
+		if leftNode == nil {
+			nonEmptyNode = rightNode
+		}
+		// Either one are nil
+		// Just make the pointer of the non-empty side point to the parent
+		if parentNode == nil {
+			// It's the root. Handle it differently
+			self.root = nonEmptyNode
+			nonEmptyNode.link["parent"] = nil
+		} else {
+			parentNode.link[parentDirn] = nonEmptyNode
+			nonEmptyNode.link["parent"] = parentNode
+		}
+	} else {
+		// Both are not nil.
+		// TODO: this is a bit tricky
+	}
+}
