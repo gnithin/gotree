58ce04f7766a2b65e72b150a036db6ad0f9d566c
gnithin <nithin.linkin@gmail.com>
2016-08-29 17:18:11 +0530

    Add examples for custom comparator

--- tree_prac.go
+++ tree_prac.go
@@ -7,11 +7,42 @@ import (
 	//"io/ioutil"
 )
 
+type myObject struct {
+	name string
+	age  int
+	sal  float64
+}
+
 func main() {
-	//tree.CreateTreeNode(1)
-	stringTree := tree.CreateTree()
+	// Testing custom objects
+
+	comparatorFunc := func(obj1, obj2 *interface{}) int {
+		new_obj1 := (*obj1).(myObject)
+		new_obj2 := (*obj2).(myObject)
+
+		// This needn't be simple
+		metric1 := new_obj1.sal + float64(20*(new_obj1.age))
+		metric2 := new_obj2.sal + float64(20*(new_obj2.age))
+
+		if metric1 < metric2 {
+			return -1
+		} else if metric1 > metric2 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	//customObjTree := tree.CreateTree()
+	customObjTree := tree.CreateTreeWithComparator(&comparatorFunc)
+	customObjTree.Insert(myObject{name: "james", age: 51, sal: 230.000})
+	customObjTree.Insert(myObject{name: "mustaine", age: 55, sal: 140.000})
+	customObjTree.Insert(myObject{name: "tom", age: 20, sal: 1240.000})
+	customObjTree.Insert(myObject{name: "jerry", age: 11, sal: 1140.000})
+
 	// Testing the strings
 	///*
+	stringTree := tree.CreateTree()
 	stringTree.Insert("hey")
 	stringTree.Insert("Oh")
 	stringTree.Insert("Listen")
