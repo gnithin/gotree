2aa1820a4360883626c7df1a37351898ac776226
gnithin <nithin.linkin@gmail.com>
2016-08-30 15:17:54 +0530

    Test out removal of nodes

--- tree_prac.go
+++ tree_prac.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"gotree/helpers"
+	//"gotree/helpers"
 	tree "gotree/tree"
-	"io/ioutil"
+	//"io/ioutil"
 )
 
 type myObject struct {
@@ -15,7 +15,7 @@ type myObject struct {
 
 func main() {
 	// Testing custom objects
-
+	///*
 	comparatorFunc := func(obj1, obj2 *interface{}) int {
 		new_obj1 := (*obj1).(myObject)
 		new_obj2 := (*obj2).(myObject)
@@ -39,6 +39,7 @@ func main() {
 	customObjTree.Insert(myObject{name: "mustaine", age: 55, sal: 140.000})
 	customObjTree.Insert(myObject{name: "tom", age: 20, sal: 1240.000})
 	customObjTree.Insert(myObject{name: "jerry", age: 11, sal: 1140.000})
+	//*/
 
 	// Testing the strings
 	///*
@@ -63,33 +64,42 @@ func main() {
 	intTree.Insert(6)
 	intTree.Insert(12)
 	intTree.Insert(4)
+
+	intTree.Remove(10)
+	intTree.Remove(1)
+	intTree.Insert(2)
+	intTree.Insert(8)
+
+	intTree.Remove(5)
+	intTree.Insert(22)
 	//*/
 
 	///*
 	checkHasVal := []int{
-		11, 121, 3, 4, 5,
+		10, 1, 5, 22, 6,
 	}
 
 	for _, val := range checkHasVal {
 		fmt.Printf("Checking if %d is in the tree - %v\n", val, intTree.HasVal(val))
 	}
 
-	jsonResp := intTree.GetJSONTree()
-	fmt.Println("Format - ", string(jsonResp))
+	/*
+		jsonResp := intTree.GetJSONTree()
+		fmt.Println("Format - ", string(jsonResp))
 
-	// write json to file
-	destFileName := "autogen.json"
-	destFilePath := "assets/data/" + destFileName
-	writeErr := ioutil.WriteFile(destFilePath, jsonResp, 0644)
-	if writeErr != nil {
-		fmt.Println(writeErr)
-		panic("Error writing json to file")
-	} else {
-		fmt.Println("Written to ", destFilePath)
-	}
+		// write json to file
+			destFileName := "autogen.json"
+			destFilePath := "assets/data/" + destFileName
+			writeErr := ioutil.WriteFile(destFilePath, jsonResp, 0644)
+			if writeErr != nil {
+				fmt.Println(writeErr)
+				panic("Error writing json to file")
+			} else {
+				fmt.Println("Written to ", destFilePath)
+			}
 
-	// Needed to display the graph
-	// TODO: Uncomment please
-	helpers.CreateServer()
-	//*/
+			// Needed to display the graph
+			// TODO: Uncomment please
+			//helpers.CreateServer()
+			//*/
 }
