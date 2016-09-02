2ff51c75aa29f8108302cf5aee5ee460311e44d0
gnithin <nithin.linkin@gmail.com>
2016-09-02 13:00:06 +0530

    [test] Add basic test for BST

--- tree/tests/BST_test.go
+++ tree/tests/BST_test.go
@@ -1,4 +1,4 @@
-package testSuite
+package tree
 
 // TODO: Benchmark stuff as well
 
@@ -8,10 +8,28 @@ import (
 	"testing"
 )
 
-func TestBST(t *testing.T) {
+func TestIntegerBST(t *testing.T) {
 	assert := assert.New(t)
-	assert.Equal(1, 1)
-	numberTree := tree.CreateBST()
-	numberTree.Insert(3, 5, 1)
 
+	numberTree := tree.CreateBST()
+	assert.True(numberTree.Insert(3, 5, 1))
+	assert.True(numberTree.InsertOne(10))
+	assert.False(numberTree.Insert(5))
+
+	// Compare the structure as well
+}
+
+func TestStringBST(t *testing.T) {
+	assert := assert.New(t)
+
+	numberTree := tree.CreateBST()
+	assert.True(numberTree.Insert(
+		"hello",
+		"hey",
+		"wazzap",
+	))
+	assert.True(numberTree.InsertOne("Ribbick"))
+	assert.False(numberTree.Insert("hey"))
+
+	// Compare the structure as well
 }
