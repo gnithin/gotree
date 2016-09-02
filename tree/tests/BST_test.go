b8c233a8239cfbed7d1d017de73c757806bea77b
gnithin <nithin.linkin@gmail.com>
2016-09-02 16:29:02 +0530

    Add tests for max and min heap

--- tree/tests/BST_test.go
+++ tree/tests/BST_test.go
@@ -1,14 +1,16 @@
-package tree
+package testSuite
 
 // TODO: Benchmark stuff as well
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"gotree/tree"
+	"sort"
 	"testing"
 )
 
-func TestIntegerBST(t *testing.T) {
+func TestBST_integer(t *testing.T) {
 	assert := assert.New(t)
 
 	numberTree := tree.CreateBST()
@@ -19,7 +21,7 @@ func TestIntegerBST(t *testing.T) {
 	// Compare the structure as well
 }
 
-func TestStringBST(t *testing.T) {
+func TestBST_string(t *testing.T) {
 	assert := assert.New(t)
 
 	numberTree := tree.CreateBST()
@@ -32,4 +34,61 @@ func TestStringBST(t *testing.T) {
 	assert.False(numberTree.Insert("hey"))
 
 	// Compare the structure as well
+}
+
+func TestBST_customObj(t *testing.T) {
+
+}
+
+func compareIntSlices(arr1, arr2 []int) bool {
+	for index, _ := range arr1 {
+		if arr1[index] != arr2[index] {
+			fmt.Println("Not matching", arr1[index], arr2[index])
+			return false
+		}
+	}
+	return true
+}
+
+func TestHeap_integer(t *testing.T) {
+	assert := assert.New(t)
+	maxHeapObj := tree.CreateMaxHeap()
+	minHeapObj := tree.CreateMinHeap()
+
+	ipArr := []int{
+		10001, 22, 1002, 101, 11,
+		32, 48, 54,
+	}
+	for _, val := range ipArr {
+		minHeapObj.Insert(val)
+		maxHeapObj.Insert(val)
+	}
+	opMinHeapArr := getHeapOp(minHeapObj)
+	opMaxHeapArr := getHeapOp(maxHeapObj)
+
+	// Compare the popped value and sorted Value
+	sort.Ints(ipArr)
+	fmt.Println(ipArr)
+	fmt.Println(opMinHeapArr)
+	assert.True(compareIntSlices(ipArr, opMinHeapArr))
+
+	sort.Sort(sort.Reverse(sort.IntSlice(ipArr)))
+	assert.True(compareIntSlices(ipArr, opMaxHeapArr))
+}
+
+func getHeapOp(heapVal *tree.Heap) []int {
+	var opArr []int
+	var poppedVal int
+	heapLen := heapVal.GetHeapLen()
+	for i := 0; i < heapLen; i++ {
+		poppedTempVal, popFlag := heapVal.Pop()
+		if poppedTempVal != nil && popFlag {
+			poppedVal = (*poppedTempVal).(int)
+			opArr = append(
+				opArr,
+				poppedVal,
+			)
+		}
+	}
+	return opArr
 }
