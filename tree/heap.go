a5625711c98bbeeb9a4634060f36f44ecc88944f
gnithin <nithin.linkin@gmail.com>
2016-09-04 15:53:13 +0530

    [heap] Clean up heap methods

--- tree/heap.go
+++ tree/heap.go
@@ -49,92 +49,30 @@ func (self *Heap) String() string {
 		&self.BaseSequentialTree)
 }
 
-/*
-	Exact same code is available in BST as well.
-	Think about making it common somehow.
-	(Macros come to mind, by golang does not have it)
-*/
-func (self *Heap) Insert(valSlice ...interface{}) bool {
-	if len(valSlice) == 0 {
-		return false
-	}
-
-	insertResp := true
-	for _, val := range valSlice {
-		insertResp = insertResp && self.InsertOne(val)
-	}
-	return insertResp
-}
-
-func (self *Heap) InsertOne(newVal interface{}) bool {
-	if self.nextInsertIndex >= self.maxSize {
-		//fmt.Println("Heap size limit reached")
-		return false
-	}
-
-	newNode := CreateTreeNode(&newVal)
-	if self.root == nil {
-		isValid := self.checkTypeForComparator(newNode)
-		if !isValid {
-			return false
-		}
-		self.root = newNode
-	}
-
-	//fmt.Println("Index - ", self.nextInsertIndex)
-	//fmt.Println("Value - ", newVal)
-
-	// Inserting into the node arr
-	self.nodeArr[self.nextInsertIndex] = newNode
-	if self.nextInsertIndex != 0 {
-		parentNode := self.nodeArr[self.getParentIndex(self.nextInsertIndex)]
-		parentDirn := "right"
-		if self.isLeftChild(self.nextInsertIndex) {
-			parentDirn = "left"
-		}
-		parentNode.link[parentDirn] = newNode
-		newNode.link["parent"] = parentNode
-
-		// Reheap up from here
-		self.reheapUp(newNode)
-
-		//fmt.Println("Dirn - ", parentDirn)
-	}
-	//fmt.Println("*********************")
-	self.len += 1
-	self.nextInsertIndex += 1
-	return true
-}
-
-func (self *Heap) reheapUp(node *Node) {
-	// Compare the present node with it's parent
-	parentNode := node.link["parent"]
-	for parentNode != nil {
-		if !self.isSizer(parentNode.data, node.data) {
-			self.swapData(parentNode, node)
-			node = parentNode
-			parentNode = node.link["parent"]
-		} else {
-			break
-		}
-	}
-}
-
+// Do not mess arround with the link pointers
+// Just change the data section inside the pointers
+// This is so that the links to the pointers are not modified
 func (self *Heap) swapData(node1, node2 *Node) {
-	// Do not mess arround with the link pointers
-	// Just change the data section
 	container := node1.data
 	node1.data = node2.data
 	node2.data = container
 }
 
+// This exchanges the pointer values
 func (self *Heap) swapPtr(node1, node2 *Node) {
 	container := node1
 	node1 = node2
 	node2 = container
 }
 
-// Used as a wrapper for the comparator for channelling in different heaps
+// Used as a wrapper for changing the comparator logic
+// depending on the type of heap used.
+// Returns true, if after comparing obj1 and obj2, it turns out
+// that obj1 is appropriately placed in the present type of heap
+// compared to obj2.
+// Naming logic ;{p
+// isSizer -> (For maxheap) isBigger, (For minHeap) isSmaller
+// Sorry, couldn't think of a better name
 func (self *Heap) isSizer(obj1, obj2 *interface{}) bool {
 	if self.comparator != nil {
 		compareResp := (*self.comparator)(obj1, obj2)
@@ -152,40 +90,14 @@ func (self *Heap) isSizer(obj1, obj2 *interface{}) bool {
 	return false
 }
 
-func (self *Heap) Pop() (*interface{}, bool) {
-	if self.len == 0 {
-		return nil, false
+func (self *Heap) reheapUp(node *Node) {
+	parentNode := node.link["parent"]
+
+	for parentNode != nil && !self.isSizer(parentNode.data, node.data) {
+		self.swapData(parentNode, node)
+		node = parentNode
+		parentNode = node.link["parent"]
 	}
-	lastElementIndex := self.nextInsertIndex - 1
-	respData := *self.root.data
-
-	// Remove the last element
-	lastElement := self.nodeArr[lastElementIndex]
-
-	self.swapData(lastElement, self.nodeArr[DEFAULT_ROOT_INDEX])
-
-	self.nodeArr[lastElementIndex] = nil
-
-	// Clean up the node references as well.
-	lastElement.link["left"] = nil
-	lastElement.link["right"] = nil
-
-	lastElementParent, isParentExists := lastElement.link["parent"]
-	if isParentExists && lastElementParent != nil {
-		parDirn := "right"
-		if self.isLeftChild(lastElementIndex) {
-			parDirn = "left"
-		}
-		lastElementParent.link[parDirn] = nil
-	}
-
-	self.len -= 1
-	self.nextInsertIndex -= 1
-
-	// Reheap down
-	self.reheapDown()
-
-	return &respData, true
 }
 
 func (self *Heap) reheapDown() {
@@ -222,6 +134,95 @@ func (self *Heap) reheapDown() {
 			needToCompareFlag = false
 		}
 	}
+}
+
+/*
+	Exact same code is available in BST as well.
+	Think about making it common somehow.
+	(Macros come to mind, by golang does not have it)
+*/
+func (self *Heap) Insert(valSlice ...interface{}) bool {
+	if len(valSlice) == 0 {
+		return false
+	}
+
+	insertResp := true
+	for _, val := range valSlice {
+		insertResp = insertResp && self.InsertOne(val)
+	}
+	return insertResp
+}
+
+func (self *Heap) InsertOne(newVal interface{}) bool {
+	if self.nextInsertIndex >= self.maxSize {
+		//fmt.Println("Heap size limit reached")
+		return false
+	}
+
+	newNode := CreateTreeNode(&newVal)
+	if self.root == nil {
+		isValid := self.checkTypeForComparator(newNode)
+		if !isValid {
+			return false
+		}
+		self.root = newNode
+	}
+
+	// Inserting into the node arr
+	self.nodeArr[self.nextInsertIndex] = newNode
+	if self.nextInsertIndex != 0 {
+		parentNode := self.nodeArr[self.getParentIndex(self.nextInsertIndex)]
+		parentDirn := "right"
+		if self.isLeftChild(self.nextInsertIndex) {
+			parentDirn = "left"
+		}
+
+		parentNode.link[parentDirn] = newNode
+		newNode.link["parent"] = parentNode
+
+		// Reheap up from here
+		self.reheapUp(newNode)
+	}
+
+	self.len += 1
+	self.nextInsertIndex += 1
+	return true
+}
+
+func (self *Heap) Pop() (*interface{}, bool) {
+	if self.len == 0 {
+		return nil, false
+	}
+	lastElementIndex := self.nextInsertIndex - 1
+	respData := *self.root.data
+
+	// Remove the last element
+	lastElement := self.nodeArr[lastElementIndex]
+
+	self.swapData(lastElement, self.nodeArr[DEFAULT_ROOT_INDEX])
+
+	self.nodeArr[lastElementIndex] = nil
+
+	// Clean up the node references as well.
+	lastElement.link["left"] = nil
+	lastElement.link["right"] = nil
+
+	lastElementParent, isParentExists := lastElement.link["parent"]
+	if isParentExists && lastElementParent != nil {
+		parDirn := "right"
+		if self.isLeftChild(lastElementIndex) {
+			parDirn = "left"
+		}
+		lastElementParent.link[parDirn] = nil
+	}
+
+	self.len -= 1
+	self.nextInsertIndex -= 1
+
+	// Reheap down
+	self.reheapDown()
+
+	return &respData, true
 }
 
 // Keeping this for the interface purpose.
