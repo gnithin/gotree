94b105956e4d2985b793100845bd2fa7e7d6a37a
gnithin <nithin.linkin@gmail.com>
2016-11-17 01:33:51 +0530

    [circle-ci] Make circle ci make a http request

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
+
+url="https://king-slayer.appspot.com/post"
+
 cd ./tree/tests/
-go test -v -bench .
+go test -v -bench . > output.out 2>&1
+
+# Storing the exit code
+exit_code="$?"
+
+all_output=$(cat output.out)
+
+# curl that resp
+curl --data "allOutput=$all_output" "$url" > /dev/null 2>&1
+
+# Exiting the code in the same way as the test resp
+exit "$exit_code"
